2206227daac3790d0b20763f4445c7fec91636be
Sanjeet Sidhu <sanjeetu3@gmail.com>
2020-09-21 19:21:03 -0700

    Enabled cors for all by adding npm package

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
+
 const URL = 'https://raw.githubusercontent.com/Sanj-sanj/DoYouEvenType/master/scripts/naruto_passages2.json';
 
 const port = process.env.PORT || 3200;
 const passage = require('./public/app')
+
+app.use(cors())
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     passage().then(resp => {res.json(resp)})
+    
 })
 
 app.listen(port, () => {
